6e1be9738071efb8c6e3b86c3bbc1f5be009471b
Jochen1980 <info@jochen-bauer.net>
2013-05-02 23:08:29 +0100

    add REST part 2

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,21 @@
  */
 
 return array(
-    // ...
+    'service_manager' => array(
+    	'factories' => array(
+    		'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+    	),
+    	'aliases' => array(
+    		'db' => 'Zend\Db\Adapter\Adapter',
+    	),
+    ),
+	'db' => array(
+		'driver'    => 'pdo',
+		'dsn'       => 'mysql:dbname=ehc;host=localhost',
+		'username'  => 'ehc',
+		'password'  => 'ehc',
+		'driver_options' => array(
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+		),
+	),
 );
