e71f795772ed78da5f4e4980620e8142e037c83e
Jochen1980 <info@jochen-bauer.net>
2013-05-03 16:56:37 +0100

    Added RestController to rep;

---
+++ module/Application/src/Application/Controller/RestController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Db\Adapter\Adapter;
+use Zend\Debug\Debug;
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
+
+class RestController extends AbstractRestfulController{
+	
+	public function getList() {
+		return array("users" => array());
+	}
+	
+	public function get($id) {
+		return array("id" => $id);
+	}
+	
+	public function create($data) {
+		return array("created" => "yes");
+	}
+	
+	public function update($id, $data) {
+		return array("updated" => "yes");
+	}
+	
+	public function delete($id) {
+		return array("deleted" => $i);
+	}
+}
