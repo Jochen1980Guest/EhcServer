7f6aaece9793d4fe783bff0455acbac3876fed8c
Jochen1980 <info@jochen-bauer.net>
2013-07-26 00:07:06 +0100

    adjust ftpupdate

--- ftpupdate.php
+++ ftpupdate.php
@@ -48,7 +48,6 @@ class FtpUpdate {
 		// Verbindung aufbauen
 		echo "* Anmeldeversuch als $this->ftpUser@$this->ftpHost\n";
 		$conn_id = ftp_connect($this->ftpHost) or die("Couldn't connect to $this->ftpHost");
-		
 		if (@ftp_login($conn_id, $this->ftpUser, $this->ftpPass)) {
 			echo "* Angemeldet als $this->ftpUser@$this->ftpHost\n";
 			echo "* Aktuelles Verzeichnis: " . ftp_pwd($conn_id) . "\n";
@@ -76,16 +75,16 @@ class FtpUpdate {
 		
 		// delete former files in root
 		$rootFiles = array(
-			"composer.json",
-			"composer.lock",
-			"composer.phar",
-			"ftpbuild.php",
-			"ftpconfig.php",
-			"ftpupdate.php",
-			"init_autoloader.php",
-			"LICENSE.txt",
-			"README.md",
-			"composer.lock",
+				"composer.json",
+				"composer.lock",
+				"composer.phar",
+				"ftpbuild.php",
+				"ftpconfig.php",
+				"ftpupdate.php",
+				"init_autoloader.php",
+				"LICENSE.txt",
+				"README.md",
+				"composer.lock",
 		);
 		
 		foreach($rootFiles as $file){
@@ -109,7 +108,7 @@ class FtpUpdate {
 				"config/autoload/global.php",
 				"config/autoload/local.php.dist",
 				"config/autoload/zfcuser.global.php",
-				"application.config.php",
+				"config/application.config.php",
 		);
 		
 		foreach($configFiles as $file){
@@ -128,23 +127,84 @@ class FtpUpdate {
 			}
 		}
 		
-		// glob() returns all file-names of a matching pattern as an array
-		// basename() return the last part of a filename
-		foreach (glob("module/*.*") as $filename){
-			if (ftp_delete($conn_id, basename($filename))) {
-				echo "* " . basename($filename) . " erfolgreich geloescht.\n";
+		// module, Application, config
+		$moduleFiles = array(
+				"module/Application/config/module.config.php",
+				"module/Application/language/de.php",
+				"module/Application/language/en.php",
+				"module/Application/src/Application/Controller/IndexController.php",
+				"module/Application/src/Application/Controller/RestController.php",
+				"module/Application/src/Application/Controller/UtilitiesController.php",
+				"module/Application/src/Application/Entity/ResidentEntity.php",
+				"module/Application/src/Application/Entity/RoomEntity.php",
+				"module/Application/src/Application/Entity/UserEntity.php",
+				"module/Application/src/Application/Filter/ContactFilter.php",
+				"module/Application/src/Application/Filter/LoginFilter.php",
+				"module/Application/src/Application/Filter/LogoutFilter.php",
+				"module/Application/src/Application/Filter/ResidentFilter.php",
+				"module/Application/src/Application/Filter/RoomFilter.php",
+				"module/Application/src/Application/Filter/StringHtmlPurifier.php",
+				"module/Application/src/Application/Filter/StringToUrl.php",
+				"module/Application/src/Application/Form/ContactForm.php",
+				"module/Application/src/Application/Form/LoginForm.php",
+				"module/Application/src/Application/Form/LogoutForm.php",
+				"module/Application/src/Application/Form/ResidentForm.php",
+				"module/Application/src/Application/Form/RoomForm.php",
+				"module/Application/src/Application/Service/ResidentService.php",
+				"module/Application/src/Application/Service/RoomService.php",
+				"module/Application/src/Application/Service/UserService.php",
+				"module/Application/src/Application/Service/UserServiceFactory.php",
+				"module/Application/view/application/index/index.phtml",
+				"module/Application/view/application/index/temp.phtml",
+				"module/Application/view/application/utilities/clear-log.phtml",
+				"module/Application/view/application/utilities/index.phtml",
+				"module/Application/view/application/utilities/show-documentation.phtml",
+				"module/Application/view/application/utilities/test.phtml",
+				"module/Application/view/error/404.phtml",
+				"module/Application/view/error/index.phtml",
+				"module/Application/view/layout/layout.phtml",
+				"module/Application/view/zfc-user/user/index.phtml",
+				"module/Application/view/zfc-user/user/login.phtml",
+				"module/Application/Module.php",
+		);	
+		
+		foreach($moduleFiles as $file){
+			// Loeschen
+			if (ftp_delete($conn_id, $file)) {
+				echo "* $file erfolgreich geloescht.\n";
 			} else {
-				echo "* Ein Fehler trat beim Loeschen von " . basename($filename) . " auf.\n";
+				echo "* Ein Fehler trat beim Loeschen von $file auf.\n";
 			}
-			
+
 			// Hochladen
-			if (ftp_put($conn_id, basename($filename), basename($filename), FTP_ASCII)) {
-				echo "* " . basename($filename) . " erfolgreich hochgeladen.\n";
+			if (ftp_put($conn_id, $file, $file, FTP_ASCII)) {
+				echo "* $file erfolgreich hochgeladen.\n";
 			} else {
-				echo "* Ein Fehler trat beim Hochladen von " . basename($filename) . " auf.\n";
+				echo "* Ein Fehler trat beim Hochladen von $file auf.\n";
 			}
 		}
-		
+			
+		$publicFiles = array(
+				"public/index.php",
+				"public/css/style.css",
+				"public/js/script.js",
+		);
+			
+		foreach($publicFiles as $file){
+			// Loeschen
+			if (ftp_delete($conn_id, $file)) {
+				echo "* $file erfolgreich geloescht.\n";
+			} else {
+				echo "* Ein Fehler trat beim Loeschen von $file auf.\n";
+			}
+				
+			// Hochladen
+			if (ftp_put($conn_id, $file, $file, FTP_ASCII)) {
+				echo "* $file erfolgreich hochgeladen.\n";
+			} else {
+				echo "* Ein Fehler trat beim Hochladen von $file auf.\n";
+			}
+		}
 	}
 	
 	
