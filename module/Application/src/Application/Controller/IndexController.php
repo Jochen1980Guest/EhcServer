b2aca3abab0b9a60c50233db854cc97545959cab
FedorSmirnov <fedorsmirnov89@gmail.com>
2013-11-05 11:41:45 +0100

    Comment as an example of a change

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -35,7 +35,7 @@ class IndexController extends AbstractActionController{
 	protected $roomService;
 	protected $residentService;
 	
-	public function setUserService(UserService $userService){
+	public function setUserService(UserService $userService){ // Kommentar
 		$this->userService = $userService;
 		return $this;
 	}
